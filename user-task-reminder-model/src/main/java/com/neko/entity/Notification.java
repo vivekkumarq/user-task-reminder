2e00da531e20a5b34096a52e04944cc697f82ed9
PraveenVilayil <praveenakuma2000@gmail.com>
2025-03-30 23:31:26 +0530

    task: added initial models task,reminder,user and notification

---
+++ user-task-reminder-model/src/main/java/com/neko/entity/Notification.java
@@ -0,0 +1,18 @@
+package com.neko.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Data
+public class Notification {
+    private UUID id;
+    private UUID userId;
+    private UUID taskId;
+    private String message;
+    private boolean seen;
+    private LocalDateTime createdDate;
+}
