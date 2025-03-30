cee4580f4a7bcca6b5226fd2ee94267411ad0903
PraveenVilayil <praveenakuma2000@gmail.com>
2025-03-30 22:51:01 +0530

    task: initial Commit

---
+++ user-task-reminder-web/src/main/java/com/neko/TaskReminderApplication.java
@@ -0,0 +1,11 @@
+package com.neko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskReminderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskReminderApplication.class);
+    }
+}
---
+++ user-task-reminder-web/src/main/java/com/neko/controller/TaskController.java
@@ -0,0 +1,13 @@
+package com.neko.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+    @GetMapping("/")
+    public String getTask() {
+        return "Hello";
+    }
+}
