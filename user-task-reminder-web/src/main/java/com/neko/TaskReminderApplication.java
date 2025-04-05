bef76be329d293ec8cd6ede15f79928b0a619148
PraveenVilayil <praveenakuma2000@gmail.com>
2025-04-05 13:20:38 +0530

    fix: changed java version and fixed build

--- user-task-reminder-web/src/main/java/com/neko/TaskReminderApplication.java
+++ user-task-reminder-web/src/main/java/com/neko/TaskReminderApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskReminderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TaskReminderApplication.class);
+        SpringApplication.run(TaskReminderApplication.class, args);
     }
 }
